12ed9199c390fd2298b2a87c7abc7a313310179e
Eelke Boezeman <eboezeman@apple.com>
2019-03-23 16:57:47 +0100

    random selected color, show selection

--- src/frontend/src/components/colorChart.js
+++ src/frontend/src/components/colorChart.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import {COLORS} from "../constants";
 import {chunk} from "lodash";
-console.log(`*** COLORS`, COLORS);
+
 const ColorChart = (props) => {
-  const {setColor} = props;
+  const {setColor, color: selectedColor} = props;
 
   const [colorArrayLeftPanel, colorArrayRightPanel] = chunk(COLORS, 10);
 
@@ -13,31 +13,39 @@ const ColorChart = (props) => {
       <div className="color-group">
         {colorArrayLeftPanel.map((color, index) => {
           return (
-            <div
-              className="color-block"
+            <Color
+              color={color}
+              selected={selectedColor === color}
               onClick={() => setColor(color)}
               key={index}
-            >
-              <div className="js-color" style={{backgroundColor: color}} />
-            </div>
+            />
           );
         })}
       </div>
       <div className="color-group">
         {colorArrayRightPanel.map((color, index) => {
           return (
-            <div
-              className="color-block"
+            <Color
+              color={color}
+              selected={selectedColor === color}
               onClick={() => setColor(color)}
               key={index}
-            >
-              <div className="js-color" style={{backgroundColor: color}} />
-            </div>
+            />
           );
         })}
       </div>
     </div>
   );
 };
+
+function Color({color, onClick, selected}) {
+  return (
+    <div onClick={onClick} className="color-block">
+      <div className="js-color" style={{backgroundColor: color}}>
+        {selected ? "🍆" : null}
+      </div>
+    </div>
+  );
+}
 
 export default ColorChart;
