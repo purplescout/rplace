9b2746885be2335eaa3d04be2f0e40e4b0047237
Eelke Boezeman <eboezeman@apple.com>
2019-03-23 16:19:19 +0100

    show coolDown toast after click

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,8 +3,12 @@ import io from "socket.io";
 import {getBoard, updateCell} from "./board";
 import {addClient, removeClient, emitClients} from "./clients";
 
+import internalIp from "internal-ip";
+import chalk from "chalk";
+
 const server = io();
-const PORT = 8000;
+const SOCKET_PORT = 8000;
+const APP_PORT = 3000;
 
 const LAST_CELL_CLICK = "LAST_CELL_CLICK";
 
@@ -28,17 +32,17 @@ server.on("connection", (client) => {
     client.emit("setBoard", state.board);
   });
   client.on("clickCell", (cell) => {
+    const now = new Date().getTime();
     if (client[LAST_CELL_CLICK]) {
       const allowedAgain =
         client[LAST_CELL_CLICK].getTime() + configuration.coolDown;
-      const now = new Date().getTime();
       const waitTime = allowedAgain - now;
       if (waitTime > 0) {
         console.log("client clicked cell within cool down, ignoring");
         client.emit("toast", {
           content: "Cool down, bro",
           options: {
-            type: "info",
+            type: "error",
             autoClose: waitTime,
           },
         });
@@ -52,7 +56,15 @@ server.on("connection", (client) => {
     state.board = updateCell(state.board, cell);
 
     emitClients("setBoard", state.board);
+    client.emit("toast", {
+      content: "Wait for it....",
+      options: {
+        type: "info",
+        autoClose: configuration.coolDown,
+      },
+    });
   });
+
   client.on("disconnect", function() {
     console.log("client disconnected");
     removeClient(client);
@@ -64,6 +76,9 @@ server.on("connection", (client) => {
   });
 });
 
-server.listen(PORT);
+server.listen(SOCKET_PORT);
 
-console.log("listening on port", PORT);
+console.log(chalk`{blue.bold Socket running on port}`, SOCKET_PORT);
+console.log(
+  chalk`{bold.green Run the game!} {white.bold.underline http://${internalIp.v4.sync()}:${APP_PORT}}`
+);
