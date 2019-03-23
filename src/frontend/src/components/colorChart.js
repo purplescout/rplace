99db2ca9cea7d686eb4d1655b07fcfd1c8fd918e
Granit Demiraj <grandemi@grandemiMacBook.local>
2019-03-23 14:36:46 +0100

    git ad PUNKT

--- src/frontend/src/components/colorChart.js
+++ src/frontend/src/components/colorChart.js
@@ -1,72 +1,61 @@
 import React from "react";
 import withColorState from "../hocs/with-color-state";
 
-const ColorChart = () => {
+const ColorChart = (props) => {
+  const {selectedColor} = props;
+
+  const colorArrayLeftPanel = [
+    "#A93226",
+    "#CB4335",
+    "#884EA0",
+    "#7D3C98",
+    "#2471A3",
+    "#2E86C1",
+    "#17A589",
+    "#138D75",
+    "#229954",
+    "#28B463",
+  ];
+  const colorArrayRightPanel = [
+    "#D4AC0D",
+    "#D68910",
+    "#CA6F1E",
+    "#BA4A00",
+    "#D0D3D4",
+    "#A6ACAF",
+    "#839192",
+    "#707B7C",
+    "#2E4053",
+    "#273746",
+  ];
+
   return (
     <div className="colorchart">
       <div className="color-group">
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#A93226"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#CB4335"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#884EA0"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#7D3C98"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#2471A3"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#2E86C1"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#17A589"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#138D75"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#229954"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#28B463"}} />
-        </div>
+        {colorArrayLeftPanel.map((color, index) => {
+          return (
+            <div
+              className="color-block"
+              onClick={() => selectedColor(color)}
+              key={index}
+            >
+              <div className="js-color" style={{backgroundColor: color}} />
+            </div>
+          );
+        })}
       </div>
       <div className="color-group">
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#D4AC0D"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#D68910"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#CA6F1E"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#BA4A00"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#D0D3D4"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#A6ACAF"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#839192"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#707B7C"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#2E4053"}} />
-        </div>
-        <div className="color-block">
-          <div className="js-color" style={{backgroundColor: "#273746"}} />
-        </div>
+        {colorArrayRightPanel.map((color, index) => {
+          return (
+            <div
+              className="color-block"
+              onClick={() => selectedColor(color)}
+              key={index}
+            >
+              <div className="js-color" style={{backgroundColor: color}} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
